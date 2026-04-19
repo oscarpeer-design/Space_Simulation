abdf3f05be8d38637858c029c9608a46113be33b
oscarpeer-design <peeroscar.personal@gmail.com>
2026-04-19 16:16:45 +1000

    Added Kepler's Law Calculations

--- Physics.h
+++ Physics.h
@@ -1,17 +1,89 @@
 #pragma once
-#include <math.h>
+#include <cmath>
+
+constexpr double UniversalGravitationalConstant = 6.674e-11; // 6.674 * 10^-11 N·m^2/kg^2
+constexpr double Pi = 3.14159265358979323846; //value for Pi for compilers that aren't C++ 20
+// Seconds per day and per Julian year (365.25 days)
+constexpr double SecondsPerDay = 24.0 * 60.0 * 60.0;
+constexpr double SecondsPerYear = 365.25 * SecondsPerDay;
 
 const int errDivisionZero = -1;
+const int errRootNegativeNumber = -2;
+const int errNumTooLarge = -3;
+
+struct Point {
+	double x = 0;
+	double y = 0;
+	double z = 0;
+};
+
+int validateDouble(double value) {
+	if (value == 0)
+		return errDivisionZero;
+	else if (value < 0)
+		return errRootNegativeNumber;
+	return 0;
+}
 
 static double ScalarGravitationalForce(double mass1, double mass2, double radius) {
 	/*This uses Newton's law of universal gravitation to calculate attraction force as a scalar 
 	F = G * ( (m1* m2) / r^2)
-	Where G is gravitational constant, m1 and m2 are masses, r is distance between centres of two objects
+	Where G is universal gravitational constant, m1 and m2 are masses, r is distance between centres of two objects
 	The Force is returned in Newtons, with m1 and m2 being in kilograms and r being in metres
 	*/
-	const double GravitationalConstant = 0.00000000006674; //6.674 * 10^-11 N/m^2/kg^2
 	if (radius == 0)
 		return errDivisionZero;
-	double force = GravitationalConstant * ( (mass1 * mass2) / (radius * radius));
+	double force = UniversalGravitationalConstant * ( (mass1 * mass2) / (radius * radius));
 	return force;
+}
+
+/*
+The following formulas for planetary motion are based on 
+1. Newton's law of universal gravitational attraction
+2. Kepler's laws of planetary attraction
+They return only scalar values.
+Note that:
+- All calculations are done in standard units (kg, N, m, seconds).
+- Note that the maximum size of a double datatype is 1.7976931348623157 * 10 ^308
+- and the minimum size of a double datatype is -1.7976931348623157 * 10 ^308
+- and the smallest possible value is 2.225 * 10 ^-308
+- So all planetary calculations should involve doubles
+*/
+
+static double ScalarOrbitalVelocity(double starMass, double radius) {
+	//This calculates the circular orbital velocity of an object around a star
+	//v = sqrt( (G *M) / r)
+	int iErr = 0;
+	if (iErr = validateDouble(radius))
+		return iErr;
+
+	double orbitalVelocity = std::sqrt( (UniversalGravitationalConstant * starMass) / radius);
+	return orbitalVelocity;
+}
+
+static double ScalarGravitationalAroundStar(double starMass, double radius) {
+	//This calculates the acceleration of an object around a star
+	//a = GM / (r^2)
+	int iErr = 0;
+	if (iErr = validateDouble(radius))
+		return iErr;
+	double acceleration = (UniversalGravitationalConstant * starMass) / (radius * radius);
+	return acceleration;
+}
+
+static double OrbitalPeriod(double starMass, double acceleration) {
+	//This calculates the time taken to complete one orbit in seconds
+	//T = 2pi * sqrt( (a ^ 3) / GM )
+	int iErr = 0;
+	if (iErr = validateDouble(starMass))
+		return iErr;
+
+	double orbitalPeriod = 2 * Pi * std::sqrt((std::pow(acceleration, 3) / (UniversalGravitationalConstant * starMass)));
+
+}
+
+static double OrbitalPeriodInYears(double orbitalPeriod) {
+	// Convert orbital period from seconds to years.
+	// Using Julian year (365.25 days) => SecondsPerYear constant above.
+	return orbitalPeriod / SecondsPerYear;
 }
